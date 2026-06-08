ad6edce99fbd12bf1b5a04c99500038cbbe66b9c
s.nechaev <s.nechaev@aitiguru.com>
2026-06-08 22:42:02 +0300

    fix: fix recomendation and add border to color
    fix recomendation and add border to color squer

--- src/entities/hooks/useRocomendation.ts
+++ src/entities/hooks/useRocomendation.ts
@@ -4,8 +4,9 @@ import { Product } from '../product/model/type';
 export const useRecomendation = (allProducts: Product[]) => {
     return useCallback((count: number) => {
         if (!allProducts || allProducts.length === 0) return [];
-        
+
         return [...allProducts]
+            .filter(product => product.colors?.some(c => c.inStore === 'true'))
             .sort(() => Math.random() - 0.5)
             .slice(0, count);
     }, [allProducts]);
